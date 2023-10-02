f138ce0dc8a8a839db8998a46b00bd88c611542e
RashelAlvarez <rashelalvarez21@gmail.com>
2023-10-01 21:12:41 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import Swiper from 'swiper';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../assets/js/bootstrap-icons/bootstrap-icons.css', 
+  '../assets/js/boxicons/css/boxicons.min.css', '../assets/js/glightbox/css/glightbox.min.css',
+  '../assets/js/swiper/swiper-bundle.min.css']
 })
 export class AppComponent {
   title = 'myportfoliov1';
+
+  
 }
+
+
+
