4e1830ab97fd95f4a50422cf236c51ba94c9b9f5
RashelAlvarez <ralvarez@intelix.biz>
2025-03-10 19:30:35 -0400

    compressor imagen

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,7 @@
 AOS.init();
 
 
-(function() {
+(function () {
   "use strict";
 
   /**
@@ -31,7 +31,7 @@ AOS.init();
   }
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
     el.addEventListener('scroll', listener)
@@ -60,7 +60,7 @@ AOS.init();
   /**
    * Scrolls to an element with header offset
    */
-   window.onscroll = function() {scrollFunction()};
+  window.onscroll = function () { scrollFunction() };
 
   function scrollFunction() {
     if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
@@ -105,7 +105,7 @@ AOS.init();
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
+  on('click', '.mobile-nav-toggle', function (e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
@@ -114,7 +114,7 @@ AOS.init();
   /**
    * Mobile nav dropdowns activate
    */
-  on('click', '.navbar .dropdown > a', function(e) {
+  on('click', '.navbar .dropdown > a', function (e) {
     if (select('#navbar').classList.contains('navbar-mobile')) {
       e.preventDefault()
       this.nextElementSibling.classList.toggle('dropdown-active')
@@ -124,7 +124,7 @@ AOS.init();
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function(e) {
+  on('click', '.scrollto', function (e) {
     if (select(this.hash)) {
       e.preventDefault()
 
@@ -180,9 +180,9 @@ AOS.init();
 
       let portfolioFilters = select('#portfolio-flters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+      on('click', '#portfolio-flters li', function (e) {
         e.preventDefault();
-        portfolioFilters.forEach(function(el) {
+        portfolioFilters.forEach(function (el) {
           el.classList.remove('filter-active');
         });
         this.classList.add('filter-active');
@@ -197,7 +197,7 @@ AOS.init();
   });
 
   /**
-   * Initiate portfolio lightbox 
+   * Initiate portfolio lightbox
    */
   const portfolioLightbox = GLightbox({
     selector: '.portfolio-lightbox'
@@ -220,10 +220,7 @@ AOS.init();
     }
   });
 
-  /**
-   * Initiate Pure Counter 
-   */
-  new PureCounter();
+
 
 })()
 
